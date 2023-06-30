f43ac9ca57fdc65a12df1fde105b1070bcba026b
Guims04 <gem.epifanio@gmail.com>
2023-06-29 21:47:13 -0300

    iniciando tratamento de erro na api

---
+++ api/src/services/errors.service.ts
@@ -0,0 +1,7 @@
+import "express-async-errors";
+import express, { NextFunction, Request, Response } from 'express';
+
+
+const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
+  
+}

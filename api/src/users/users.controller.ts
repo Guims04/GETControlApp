b9ad7989516cd8c710126fb40f5b2c7be5380344
guims04 <gem.epifanio@gmail.com>
2023-07-05 21:02:13 -0300

    Refatorando e documentando users

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,16 +1,22 @@
 import { Request, Response, request } from "express";
 import { UsersService } from "./users.service";
 
+// instances
+const usersService = new UsersService;
 
 class UsersController{
+
+  // create user controller
   async createUser(req: Request, res: Response ){
     const { username, name, password } = req.body;
-    const auth = new UsersService;
-    const user = await auth.create({
+
+    const user = await usersService.create({
       username, name, password,
     });
+    
     return res.json(user);
   }
+
 }
 
 export { UsersController }
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -2,16 +2,19 @@ import { client } from "../prisma/client";
 import { hash } from "bcryptjs";
 import createHttpError from 'create-http-error';
 
-
+// Interfaces
 interface IUser{
     name:     string;
     password: string;
     username: string;
 }
 
+// Class
 class UsersService{
+
+  // Create user function
   async create({name,username,password}:IUser){
-    // Verificar se usuário existe
+    // Check if user exists
     const user = await client.user.findFirst({
       where:{
         username
@@ -21,7 +24,8 @@ class UsersService{
     if(user){
       throw createHttpError(409, "User already exists!");
     }    
-    //Cadastra usuário
+
+    //Create user with cripted password
     const passwordHash = await hash(password,8)
     return await client.user.create({
       data:{
@@ -29,7 +33,7 @@ class UsersService{
         username,
         password: passwordHash,
       }
-    })
+    });
   }
     
 }

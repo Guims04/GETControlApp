e4094bd1a816e565693f534afbb8f2bc691edaac
guims04 <gem.epifanio@gmail.com>
2023-07-10 22:59:46 -0300

    criando renovação de token

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -7,15 +7,27 @@ const authService = new AuthService();
 // Class
 class AuthController{
 
+  // Authenticate user
   async login(req: Request, res: Response){
     const { username, password } = req.body;
 
-    const token = await authService.authenticate({
+    const access = await authService.authenticate({
       username,
       password
     })
+
+    return res.json(access);
+  }
+
+  // Renew token
+  async renewToken(req: Request, res: Response){
+    const { refreshToken } = req.body
+
+    const token = await authService.renewToken(refreshToken)
+
     return res.json(token);
   }
+
 }
 
 export { AuthController };
--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import httpStatus from "http-status";
 import { compare } from "bcryptjs";
 import { sign, verify } from "jsonwebtoken";
 import { jwtSecret } from '../api.config';
+import moment from 'moment';
 
 // Interfaces
 interface IAuth{
@@ -12,17 +13,28 @@ interface IAuth{
   password: string;
 }
 
+interface Itoken{
+  userId: number;
+}
+
 // Class
 class AuthService{
+
+  // generate token
+  generateToken(data: Itoken){
+    return sign({ 
+      subject: data
+    },jwtSecret,{ expiresIn: "20s" });
+  }
 
   // Authenticate User
   async authenticate({ username, password }: IAuth){
     
     // check if user exists
     const user = await client.user.findFirst({
-     where:{
-      username,
-     }, 
+      where:{
+        username,
+      }, 
     });
 
     if(!user) throw createHttpError(httpStatus.BAD_REQUEST, "User or password incorrect!");
@@ -33,13 +45,12 @@ class AuthService{
     if(!passMatch) throw createHttpError(httpStatus.BAD_REQUEST, "User or password incorrect!");
 
     // genarete user token
-    const token = sign({      
-      subject: user.id    
-    },jwtSecret,{
-      expiresIn: "20s"  
-    });
+    const token = this.generateToken({ userId: user.id });
+    
+    // create refresh token
+    const refreshToken = await this.createRefreshToken(user.id);
 
-    return {token};
+    return { token, refreshToken };
   }
 
   // validate token
@@ -47,16 +58,47 @@ class AuthService{
     const authToken = req.headers.authorization;
 
     if(!authToken) throw createHttpError(httpStatus.BAD_REQUEST,"Token is missing");
-  // split Bearer to get token
-  const [,token] = authToken.split(" ");
-  try {
-    verify(token,jwtSecret);    
-    return next();
-  } catch (error) {
-    throw createHttpError(httpStatus.BAD_REQUEST, error);
-  }
+    // split Bearer to get token
+    const [,token] = authToken.split(" ");
+    try {
+      verify(token,jwtSecret);
+      return next();
+    } catch (error) {
+      throw createHttpError(httpStatus.BAD_REQUEST, error);
+    }
 
   }
+
+  // create refresh token in table
+  async createRefreshToken(userId: number){
+    // generate a moment expires
+    const expiresIn = moment().add(15, 'minutes').unix();
+
+    console.log(userId);
+    console.log(expiresIn);
+    // create refresh token
+    const refresh_token = await client.refreshToken.create({
+      data: { userId, expiresIn }
+    })
+    console.log(refresh_token);
+
+    return refresh_token;
+  }
+
+  // Renew token
+  async renewToken(refresh_token: string){
+
+    const refreshToken = await client.refreshToken.findFirst({
+      where: { id: refresh_token }
+    })
+
+    if (!refreshToken) throw createHttpError(httpStatus.BAD_REQUEST, "Invalid refresh token");
+
+    const token = this.generateToken({ userId: refreshToken.userId });
+
+    return { token }
+  }
+
 }
 
 export { AuthService };

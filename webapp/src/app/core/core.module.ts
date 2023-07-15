ddfb51d0b446ecb2d3f1095e71f5d28d98d0110f
JorgenMardens <jorgen.emardens@gmail.com>
2023-07-14 21:59:22 -0300

    pequenos ajustes

--- webapp/src/app/core/core.module.ts
+++ webapp/src/app/core/core.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderSearchComponent } from './components/header/header-search/header-search.component';
+import { HeaderMessagesComponent } from './components/header/header-messages/header-messages.component';
+import { HeaderNotificationsComponent } from './components/header/header-notifications/header-notifications.component';
 
 
 
@@ -10,7 +13,10 @@ import { FooterComponent } from './components/footer/footer.component';
   declarations: [
     HeaderComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderSearchComponent,
+    HeaderMessagesComponent,
+    HeaderNotificationsComponent
   ],
   imports: [
     CommonModule

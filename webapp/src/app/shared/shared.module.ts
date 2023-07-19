bbf91d5c92a9abb1a4ee3e3f33b94d45503f8a3c
JorgenMardens <jorgen.emardens@gmail.com>
2023-07-18 22:31:09 -0300

    criando content header e modulo shared

---
+++ webapp/src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContentHeaderComponent } from './components/content/content-header/content-header.component';
+
+@NgModule({
+  declarations: [ContentHeaderComponent],
+  imports: [CommonModule],
+  exports: [ContentHeaderComponent],
+})
+export class SharedModule {}

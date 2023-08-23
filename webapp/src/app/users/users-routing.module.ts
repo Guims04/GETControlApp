2d8341c10ac5e2b76459603cc0a2963ef148f12f
JorgenMardens <jorgen.emardens@gmail.com>
2023-08-22 21:06:45 -0300

    iniciando form de usuario

--- webapp/src/app/users/users-routing.module.ts
+++ webapp/src/app/users/users-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './users.component';
 import { UsersListComponent } from './components/users-list/users-list.component';
+import { UsersFormComponent } from './components/users-form/users-form.component';
 
 const routes: Routes = [
   {
     path: '',
     component: UsersComponent,
-    children: [{ path: '', component: UsersListComponent }],
+    children: [
+      { path: '', component: UsersListComponent },
+      { path: 'new', component: UsersFormComponent },
+      { path: ':userId', component: UsersFormComponent },
+    ],
   },
   {
     path: '**',
